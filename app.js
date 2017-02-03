68c1f655bd41b03b80fe6aa0f1c30c2c06de08bd
Patrick McDavid <patrick@bombbomb.com>
2017-02-03 15:45:16 -0700

    switched to handlebars

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ let favicon = require('serve-favicon');
 let logger = require('morgan');
 let cookieParser = require('cookie-parser');
 let bodyParser = require('body-parser');
+let exphbs  = require('express-handlebars');
 
 let db = require('./classes/db');
 db.init();
@@ -15,8 +16,8 @@ let team = require('./routes/team');
 let app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
